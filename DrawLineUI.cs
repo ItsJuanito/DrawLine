4edeafa27cd4e66324e5bb8602d34169b402b68f
Juan Zaragoza <56614909+ItsJuanito@users.noreply.github.com>
2022-10-18 13:05:46 -0700

    remove unwanted comments

--- DrawLineUI.cs
+++ DrawLineUI.cs
@@ -1,17 +1,3 @@
-//****************************************************************************************************************************
-//Program name: "Ninety Degree Turn 3.0".  This programs accepts the coordinates of two points from the user, draws a        *
-//straight line segment connecting them, and ball travels from the beginning end point to the terminal end point.            *
-//Copyright (C) 2022  Floyd Holliday                                                                                         *
-//                                                                                                                           *
-//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-//version 3 as published by the Free Software Foundation.                                                                    *
-//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
-//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
-//A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
-//****************************************************************************************************************************
-
-
-
 ﻿//Ruler:=1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
 //Author: Juan Zaragoza
 //Mail: zaragoza_9@csu.fullerton.edu
